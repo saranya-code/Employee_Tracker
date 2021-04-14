fc30f3281ffad97c9e7307335082bbf9bce6f42a
saranya-code <saranyadayalan28@gmail.com>
2021-04-13 22:05:50 -0500

    removed root password

--- js/creds.js
+++ js/creds.js
@@ -4,7 +4,7 @@ module.exports = {
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "rootroot",
+        password: "",
         database: "employee_tracker_DB"
     })
 }
